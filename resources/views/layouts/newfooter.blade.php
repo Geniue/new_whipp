042d3f0d2f35e30cab8cefbfad9fe9afae2542da
vinith <vinithdevadiga7@.com>
2023-08-17 18:45:04 +0530

    new footer address

--- resources/views/layouts/newfooter.blade.php
+++ resources/views/layouts/newfooter.blade.php
@@ -161,7 +161,14 @@
 
             <div class=" col-9 col-md-5 col-lg-auto d-flex flex-column gap-2  ">
                 <p class="fs-5 fw-bold mb-0">CONTACT</p>
-                <p class="mb-0" style="font-size: 14px">Be the first to know about existing news, special events and much more.</p>
+
+
+                <p class="mb-0 fw-semibold">
+                    46 Howard St 6th fl,
+                    <br>
+                     New York
+                     <br>
+                      NY 10013</p>
                 {{-- form --}}
                 {{-- <form class="d-flex flex-column gap-3 gap-lg-2">
                     <div class="position-relative overflow-hidden">
@@ -177,9 +184,9 @@
             <a href="mailto:support@whippdigital.com" class="">support@whippdigital.com</a>
             <a href="tel:212-951-0651" class="">212-951-0651</a>
             <div class="d-flex align-items-center mt-2">
-                <div class="footer-loc-soc-link ms-1 d-flex gap-3 gap-lg-2 gap-xl-3 ">
+                <div class="footer-loc-soc-link ms-1 d-flex gap-2 gap-lg-2 gap-xl-3 ">
                     <a  href="#">
-                        <svg width="30" height="30" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="25" height="25" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
                             <g fill="none" fill-rule="evenodd">
                             <path d="M25 13c0 6.628-5.372 12-12 12S1 19.628 1 13 6.372 1 13 1s12 5.372 12 12z" stroke="#FFF" stroke-width="1.978"></path>
                             <path d="M8.46 16.243h2.594v8.585c.636.104 1.282.172 1.946.172.664 0 1.31-.068 1.946-.172v-8.585h2.351a.648.648 0 0 0 .644-.567l.243-1.946a.654.654 0 0 0-.157-.51.645.645 0 0 0-.486-.22h-2.595v-2.27a1.3 1.3 0 0 1 1.297-1.298h1.298a.648.648 0 0 0 .648-.648v-2.19a.648.648 0 0 0-.59-.646c-.038-.002-.953-.083-2.11-.083-2.861 0-4.435 1.698-4.435 4.779V13H8.46a.648.648 0 0 0-.648.649v1.946c0 .36.291.648.648.648z" fill="#FFF" fill-rule="nonzero"></path>
@@ -187,7 +194,7 @@
                             </svg>
                         </a>
                         <a  href="#">
-                            <svg width="30" height="28" viewBox="0 0 28 24" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="25" height="25" viewBox="0 0 28 24" xmlns="http://www.w3.org/2000/svg">
                                 <g fill="none" fill-rule="evenodd">
                                 <path d="M27.812 3.17a.676.676 0 0 0-.758-.14l-.11.049a9.136 9.136 0 0 1-.278.125c.27-.426.487-.88.635-1.352a.65.65 0 0 0-.226-.713.672.672 0 0 0-.756-.043c-1.049.628-2.038 1.018-3.117 1.23l-.523.088c1.404 1.016 2.32 2.65 2.32 4.497 0 .085-.005.172-.005.257l.099-.08c2.083-1.711 2.805-3.11 2.836-3.165a.647.647 0 0 0-.117-.752z" fill="#FFF" fill-rule="nonzero"></path>
                                 <path d="M19.333 1.33c-3.13 0-5.667 2.498-5.667 5.581v.985C5.54 7.468 3.333 1.986 3.333 1.986s-1 1.313-1 3.283C2.333 8.553 5 9.866 5 9.866c-2 0-3.333-.657-3.333-.657S2 12.821 6.333 14.463l-2.666.657S5 17.418 9 18.403c0 0-2.334 1.97-8 1.97C1 20.373 3.333 23 9.666 23 20.666 23 25 12.821 25 6.911c0-3.083-2.536-5.582-5.666-5.582z" stroke="#FFF" stroke-width="1.978" stroke-linejoin="round"></path>
@@ -198,7 +205,7 @@
                         <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35px" height="35px"> <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path></svg>
                     </a>
                     <a  href="#">
-                        <svg width="30" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="25" height="25" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <g fill="none" fill-rule="evenodd">
                             <path d="M20.333 23H3.667A2.665 2.665 0 0 1 1 20.333V3.667A2.665 2.665 0 0 1 3.667 1h16.666A2.665 2.665 0 0 1 23 3.667v16.666A2.665 2.665 0 0 1 20.333 23z" stroke="#FFF" stroke-width="1.978"></path>
                             <path d="M8 6.333C8 7.253 7.253 8 6.333 8c-.919 0-1.666-.747-1.666-1.667 0-.919.747-1.666 1.666-1.666.92 0 1.667.747 1.667 1.666zM7.333 19.333h-2a.665.665 0 0 1-.666-.666V10c0-.37.297-.667.666-.667h2c.37 0 .667.297.667.667v8.667c0 .37-.297.666-.667.666zM15.333 9.333a3.98 3.98 0 0 0-2.666 1.026V10A.665.665 0 0 0 12 9.333h-2a.665.665 0 0 0-.667.667v8.667c0 .37.297.666.667.666h2c.37 0 .667-.297.667-.666v-5c0-.92.747-1.667 1.666-1.667.92 0 1.667.747 1.667 1.667v5c0 .37.297.666.667.666h2c.37 0 .666-.297.666-.666v-5.334c0-2.205-1.794-4-4-4z" fill="#FFF" fill-rule="nonzero"></path>
