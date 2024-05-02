a9fa22bf6450911b5aa24179f264598e2812c24f
Geniue <mostagenius@gmail.com>
2024-05-02 23:18:58 +0300

    removing fees from stripe8

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -90,6 +90,8 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'uniqId' => ['required']
+        ],[
+            'password.min' => 'Please use a longer password',
         ]);
 
         $inv = InvitedUsersModel::where('uniq_id', $data['uniqId'])->get()[0] ?? abort(404);
--- resources/views/auth/register_invited_users.blade.php
+++ resources/views/auth/register_invited_users.blade.php
@@ -47,7 +47,7 @@
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ dd($errors->all()) }}</strong>
+                                        <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
