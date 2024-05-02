5729ae448683f48191392501f928f26308fd72ae
Geniue <mostagenius@gmail.com>
2024-05-03 02:55:27 +0300

    adding logo to emails

--- resources/views/user/emails/user-invite-email.blade.php
+++ resources/views/user/emails/user-invite-email.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-# Welcome to Whippdigital!
+# Welcome to Whipp Digital!
 
 You have been invited to setup an account at Whipp Digital. Please click the button below to accept the invitation and set up your account.
 
@@ -10,5 +10,5 @@ Accept Invitation
 If you did not expect this invitation or believe it was sent in error, please ignore this email.
 
 Thank you for your attention,<br>
-The {{ config('app.name') }} Team
+<a href="https://whippdigital.com">WhippDigital.com</a>
 @endcomponent
