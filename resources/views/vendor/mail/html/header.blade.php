e528ff15d3a177d156ac9fd443ededf7e902add7
Geniue <mostagenius@gmail.com>
2024-05-03 03:14:08 +0300

    adding email icon2

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="https://whippdigital.com/assets/imgs/logos/emailicon.png" class="logo" alt="Whipp Digital Logo">
+<img src="https://whippdigital.com/assets/imgs/emailicon.png" class="logo" alt="Whipp Digital Logo">
 @endif
 </a>
 </td>
