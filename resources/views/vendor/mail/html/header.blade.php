f25ad82b3eb90719c0398253c6f94709958b17b8
Geniue <mostagenius@gmail.com>
2024-05-06 18:28:01 +0300

    launch v1 -10

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="https://whippdigital.com/assets/imgs/logos/maximized_whipplogo.png" class="logo" alt="Whipp Digital Logo" width="150" height="150">
+<img src="https://whippdigital.com/assets/imgs/logos/maximized_whipplogo_1.png" class="logo" alt="Whipp Digital Logo" width="150" height="150" style="height: 175px!important; width: 175px !important; max-height: unset!important;">
 @endif
 </a>
 </td>
